e31a7d842b6469dcdf1ffe7469cf1714d2dd66db
Jon Ester <jonesterwebdev@gmail.com>
2021-09-21 14:37:38 -0400

    Update our menu-items withRouter HOC

--- ecommerce-portfolio/src/components/directory/directory.component.jsx
+++ ecommerce-portfolio/src/components/directory/directory.component.jsx
@@ -46,8 +46,8 @@ class Directory extends React.Component {
   render(){
     return (
       <div className ="directory-menu">
-        {this.state.sections.map(({title, imageUrl, id, size}) => (
-            <MenuItem imageUrl = {imageUrl} title = {title} key = {id} size = {size}/>
+        {this.state.sections.map(({title, imageUrl, id, size, linkUrl}) => (
+            <MenuItem imageUrl = {imageUrl} title = {title} key = {id} size = {size} linkUrl ={linkUrl}/>
           ))
         }
       </div>
