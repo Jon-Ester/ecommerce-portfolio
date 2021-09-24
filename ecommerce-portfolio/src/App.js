736e7cf2520a258c863ca8bfb464831d1f4b33d8
Jon Ester <jonesterwebdev@gmail.com>
2021-09-24 09:32:01 -0400

    completed sign-in styling and js along with custom button component

--- ecommerce-portfolio/src/App.js
+++ ecommerce-portfolio/src/App.js
@@ -2,14 +2,17 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component'
-
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in&up/sign-in&up.component';
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <Switch>
         <Route exact path ="/" component ={HomePage}/>
         <Route path ="/shop" component ={ShopPage}/>
+        <Route path ="/signin" component ={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
---
+++ ecommerce-portfolio/src/components/form-input/form-input.component.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './form-input.styles.scss';
+
+const FormInput = ({handleChange, label, ...otherProps }) =>(
+<div className ="group">
+  <input className ="form-input" onChange ={handleChange}{...otherProps}/>
+  {
+    label ?
+    (<label className ={`${otherProps.value.length ? 'shrink' : ''} form-input-label `} type ="text" for ="email"> {label} </label>)
+    : null
+  }
+</div>
+
+)
+
+export default FormInput;
